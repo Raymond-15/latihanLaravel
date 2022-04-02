1dd520bbef1330649a2ca2581f8aa4984f2ffe63
Raymond-15 <raymondputra15@gmail.com>
2022-04-02 10:45:41 +0800

    create controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -28,14 +29,7 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-
-
-    return view('posts', [
-        'judul' => 'Posts',
-        'posts' => Post::all()
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 Route::get('posts/{slug}', function ($slug) {
 
